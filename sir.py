eb78356bbfeb66b41bc5b276e553c223539616f0
Bernardo Junior <bernardodemsj@gmail.com>
2019-07-26 03:03:45 -0300

    ''

--- sir.py
+++ sir.py
@@ -1,32 +1,57 @@
-#TODO: funções sir para o problema
+#article: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC29087/
 class sir_model:
     def __init__(self):
-        pass
-        # State Variables	
-        #     S	number of susceptibles
-        #     I	number of infected
-        #     B	concentration of toxigenic V. cholerae in water (cells/ml)
-        # Parameters	
-        #     H	total human population
-        #     n	Human birth and death rates (day-1)
-        #     a	rate of exposure to contaminated water (day-1)
-        #     K	concentration of V. cholerae in water that yields 50% chance of
-        #         catching cholera (cells/ml)
-        #     r	rate at which people recover from cholera (day-1)
-        #     nb	growth rate of V. cholerae in the aquatic environment (day-1)
-        #     mb	loss rate of V. cholerae in the aquatic environment (day-1)
-        #     e	contribution of each infected person to the population of V. cholerae
-        #         in the aquatic environment (cell/ml day-1 person-1)
+        self.__c1()
+        self.S = self.H
+        self.I = 1
+        self.B = 0
+        print(self.λ())
+        print(self.dS_dt())
+        print(self.dI_dt())
+        print(self.dB_dt())
     
-    def dS_dt(self, n, H, S, a, B, K, s):
-        return n * (H - S) - a * self.λ(B, K) * S
+    def dS_dt(self):
+        return self.n * (self.H - self.S) - self.a * self.λ() * self.S 
     
-    def dI_dt(self, a, B, K, S, r, I):
-        return a * self.λ(B, K) * S - (r * I)
+    def dI_dt(self):
+        return self.a * self.λ() * self.S - (self.r * self.I)
 
-    def dB_dt(self, B, nb, mb, e, I):
-        return ( B * (nb - mb)) + (e * I)
+    def dB_dt(self):
+        return ( self.B * self.nb_mb) + (self.e * self.I)
 
-    def λ(self, B, K):
-        return B / (K + B)
+    def λ(self):
+        return self.B / (self.K + self.B)
 
+
+    def __c1(self):
+        self.H = 10_000
+        self.n = 0.0001
+        self.a = 0.5
+        self.K = 1_000_000
+        self.r = 0.2
+        self.nb_mb = -0.33
+        self.e = 10
+        self.SC = 13_200
+    
+    def __c2(self):
+        self.H = 10_000
+        self.n = 0.0001
+        self.a = 1
+        self.K = 1_000_000
+        self.r = 0.2
+        self.nb_mb = -0.33
+        self.e = 10
+        self.SC = 6_600
+    
+    def __c2(self):
+        self.H = 10_000
+        self.n = 0.001
+        self.a = 1
+        self.K = 1_000_000
+        self.r = 0.2
+        self.nb_mb = -0.33
+        self.e = 10
+        self.SC = 6_600
+
+if __name__ == "__main__":
+    a = sir_model()
